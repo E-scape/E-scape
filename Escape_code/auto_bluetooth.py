ab1c72b8e58087d09097f215844bcc20c9bb7f6d
Jeon Minkwon <115460992+cosa1ne@users.noreply.github.com>
2024-08-30 19:47:18 +0900

    블루투스

---
+++ Escape_code/auto_bluetooth.py
@@ -0,0 +1,33 @@
+import subprocess
+import time
+
+def connect():
+    subprocess.run("gnome-terminal -- bash -c 'sudo rfcomm connect escape 98:DA:60:07:C6:AC 1; exec bash'", shell=True)
+    subprocess.run("gnome-terminal -- bash -c 'sudo rfcomm connect escape 98:DA:60:07:C6:AC 1; exec bash'", shell=True)#이어폰
+
+def check():
+    output = subprocess.check_output("hcitool con", shell=True, universal_newlines=True)
+    
+    if "98:DA:60:07:C6:AC" in output:
+        return True
+    else:
+        print("reconnecting")
+        return False
+    
+connect()
+time.sleep(5)
+subprocess.run("gnome-terminal -- bash -c 'python3 /home/escape/escape_project/listen_bt.py; exec bash'", shell=True)#bluetooth.py로 수정
+time.sleep(1)
+
+while True:
+    if not check():
+        connect()
+        time.sleep(5)
+        subprocess.run("pkill gnome-terminal", shell=True, check=True)
+
+        if check() == True:    
+            subprocess.run("gnome-terminal -- bash -c 'python3 /home/escape/escape_project/listen_bt.py; exec bash'", shell=True)#bluetooth.py로 수정
+        else:
+            continue
+
+    time.sleep(10)
